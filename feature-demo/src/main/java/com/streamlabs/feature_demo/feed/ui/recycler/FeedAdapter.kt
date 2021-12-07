872d6c6dbde5763d973af5a34ac29ef1505ed356
Egor Chernetsov <ext.echernetsov1@tinkoff.ru>
2021-12-07 05:57:55 +0300

    load Video Using ExoPlayer

--- feature-demo/src/main/java/com/streamlabs/feature_demo/feed/ui/recycler/FeedAdapter.kt
+++ feature-demo/src/main/java/com/streamlabs/feature_demo/feed/ui/recycler/FeedAdapter.kt
@@ -3,6 +3,8 @@ package com.streamlabs.feature_demo.feed.ui.recycler
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 import com.streamlabs.feature_demo.databinding.FeedItemBinding
 import com.streamlabs.feature_demo.feed.ui.model.VideoUi
 
@@ -17,7 +19,10 @@ class FeedAdapter : RecyclerView.Adapter<FeedAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = FeedItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+        val playerView = ExoPlayer.Builder(parent.context)
+            .build()
+
+        return ViewHolder(binding, playerView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -26,14 +31,21 @@ class FeedAdapter : RecyclerView.Adapter<FeedAdapter.ViewHolder>() {
             userNameText.text = items[position].userName
             commentsNumber.text = items[position].videoNumberComments.toString()
             likesNumber.text = items[position].videoNumberLikes.toString()
-            videoView.setVideoPath(items[position].videoPath)
-            videoView.start();
+            videoView.player = holder.playerView
+
+            val mediaItem: MediaItem = MediaItem.fromUri(items[position].videoPath!!)
+            holder.playerView.setMediaItem(mediaItem)
+            holder.playerView.prepare()
+            holder.playerView.play()
         }
     }
 
     override fun getItemCount() = items.size
 
 
-    class ViewHolder(val binding: FeedItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder(
+        val binding: FeedItemBinding,
+        val playerView: ExoPlayer
+    ) : RecyclerView.ViewHolder(binding.root) {
     }
 }
